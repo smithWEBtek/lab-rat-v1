3b2e29bee941b881903a147c9037c493fd93076c
smithwebtek <brad@smithwebtek.com>
2017-04-23 10:41:42 -0400

    json data in the lab-rat

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-
-  resources :picture_uploaders
+  
   root 'tools#home'
-  get '/contacts/:id/detail', to: 'contacts#detail'
- 
+  
   get '/reset_pdfs', to: 'pdfs#reset_pdfs'
   get '/import_pdfs', to: 'pdfs#import_pdfs'
   get '/download_pdf/:id', to: 'pdfs#download_pdf'
   get '/new_distill_pdf', to: 'pdfs#new_distill_pdf'
   post '/distill_pdf', to: 'pdfs#distill_pdf'
 
-  resources :companies, :contacts, :tools, :pdfs, :docs
+  resources :companies, :contacts, :tools, :pdfs, :docs, :picture_uploaders
 
   # get '/bookmarks', to: 'tools#bookmarks'
   # get '/test', to: 'tools#test'
